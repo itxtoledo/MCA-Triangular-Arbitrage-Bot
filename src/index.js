b26b5f69247e677adba4ece4cf1d4c95328baba7
Gustavo <gugurock50@gmail.com>
2019-08-03 14:38:52 -0300

    test with websocket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import Binance from 'binance-api-node';
 import WebSocketAsPromised from 'websocket-as-promised';
+import { w3cwebsocket } from 'websocket'
 
+const wsp = new WebSocketAsPromised(
+    'wss://stream.binance.com:9443/ws/!ticker@arr',
+    {
+        createWebSocket: url => new w3cwebsocket(url)
+    }
+);
+
+let markets = [
+    'BNB', 'BTC', 'ETH', 'XRP', 'USDT', 'PAX', 'TUSD', 'USDC', 'USDS'
+]
+
+wsp.open().then((result) => {
+    wsp.onMessage.addListener(message => {
+        findArbitrageAndShow(JSON.parse(message));
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+function findArbitrageAndShow(tickers){
+    // console.log(tickers);
+    let tempTickers = {};
+    let tempMarket = '';
+    for (let i = 0; i < tickers.length; i++) {
+        tempMarket = endsWArray(tickers[i].s);
+        // console.log(tempMarket);
+        tempTickers[tempMarket] = tickers[i];
+    }
+
+    console.log(tempTickers);
+}
+
+function endsWArray(stringToVerify){
+    // console.log(stringToVerify);
+    for (let i = 0; i < markets.length; i++) {
+        if (stringToVerify.endsWith(markets[i]))
+            return markets[i];
+    }
+}
